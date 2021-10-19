64a18223973964ee50e97bd8b8f9b3b5ac10a990
pleexyy <thibaud.bonneville@gmail.com>
2021-10-19 14:20:17 +0200

    premier design de l'application Java

---
+++ src/launch/Launch.java
@@ -0,0 +1,19 @@
+package launch;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launch extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/window.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Log abstraction v1.0");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
---
+++ src/view/mainWindow.java
@@ -0,0 +1,26 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mainWindow implements Initializable {
+
+    @FXML
+    private TextField tflink;
+
+    @FXML
+    private Button btnconfirm;
+
+    @FXML
+    private Button btnbrowse;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
