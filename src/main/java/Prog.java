4e0dd97f60c72585cf49338ee4266b71cd556124
Thibaud <thibaud.bonneville@gmail.com>
2021-11-09 09:23:37 +0100

    maj matricedistancefactory

--- src/main/java/Prog.java
+++ src/main/java/Prog.java
@@ -1,5 +1,6 @@
 import factory.MatriceDistanceFactory;
 import factory.TableauFactory;
+import model.AbstacteurSession;
 import model.ConversationSet;
 import model.Tableau;
 import parser.LogParser;
@@ -24,7 +25,7 @@ public class Prog {
                 throw new IOException("L'URL de la ressource est null");
             }
             Tableau t = TableauFactory.createTableau(convSet);
-            Matrix m = MatriceDistanceFactory.createMatrixDistance(t, convSet, 1, facteurAttenuation);
+            Matrix m = MatriceDistanceFactory.createMatrixDistance(t, new AbstacteurSession().abstracteur(convSet), 1, facteurAttenuation);
             System.out.println(m);
             var dbscan = DBSCAN.fit(m.toArray(), 1, 1);
             System.out.println("nombre de cluster(s) pour un facteur d'atténuation de " + facteurAttenuation + " : " + dbscan.k);
--- src/main/java/factory/MatriceDistanceFactory.java
+++ src/main/java/factory/MatriceDistanceFactory.java
@@ -1,18 +1,18 @@
 package factory;
 
-import model.Conversation;
-import model.ConversationSet;
-import model.Event;
+import model.SessionAbstract;
 import model.Tableau;
 import smile.math.matrix.Matrix;
 
+import java.util.List;
+
 public class MatriceDistanceFactory {
 
-    public static Matrix createMatrixDistance(Tableau t, ConversationSet convSet, final int lookback, final double facteurAttenuation) {
-        return createMatrixDistance(t, convSet, lookback, 1, facteurAttenuation);
+    public static Matrix createMatrixDistance(Tableau t, List<SessionAbstract> sessionAbstracts, final int lookback, final double facteurAttenuation) {
+        return createMatrixDistance(t, sessionAbstracts, lookback, 1, facteurAttenuation);
     }
 
-    public static Matrix createMatrixDistance(Tableau t, ConversationSet convSet, final int lookback, final int incrementValue, final double facteurAttenuation) {
+    public static Matrix createMatrixDistance(Tableau t, List<SessionAbstract> sessionAbstracts, final int lookback, final int incrementValue, final double facteurAttenuation) {
         if (lookback < 0 || incrementValue < 0)
             throw new IllegalArgumentException("loopback ou incrementValue est négatif");
         if (facteurAttenuation <= 0 || facteurAttenuation > 1)
@@ -22,16 +22,15 @@ public class MatriceDistanceFactory {
             int tailleColumn = t.getSizeColumns();
             m = new Matrix(tailleColumn, tailleColumn);
         }
-        for (Conversation conv : convSet.getConversationSet()) {
-            int size = conv.size();
-            for (int i = 0; i < size; i++) {
-                Event refEvent = conv.getConv().get(i);
-                int rowIndex = t.columnIndexOf(refEvent.getEventWithTypes());
+        for (SessionAbstract sabs : sessionAbstracts) {
+            for (int i = 0; i < sabs.getTypage().size(); i++) {
+                String ref = sabs.getTypage().get(i);
+                int rowIndex = t.columnIndexOf(ref);
                 for (int j = 1; j <= lookback; j++) {
                     if (i >= j) {
-                        Event eventY = conv.getConv().get(i - j);
+                        String eventY = sabs.getTypage().get(i - j);
                         // on trouve l'emplacement où prendre et mettre la valeur
-                        int columnIndex = t.columnIndexOf(eventY.getEventWithTypes());
+                        int columnIndex = t.columnIndexOf(eventY);
                         // on obtient la distance
                         double distanceValue = 1.0 / m.get(rowIndex, columnIndex) + incrementValue * Math.pow(facteurAttenuation, j);
 
