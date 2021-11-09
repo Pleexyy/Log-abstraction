275b5be67ebb942c515b2da31abfd5d814908e73
Amiralgaby <gaby.theuws@gmail.com>
2021-11-09 10:12:39 +0100

    suppression tableau

--- src/main/java/Prog.java
+++ src/main/java/Prog.java
@@ -1,8 +1,6 @@
 import factory.MatriceDistanceFactory;
-import factory.TableauFactory;
 import model.AbstacteurSession;
 import model.ConversationSet;
-import model.Tableau;
 import parser.LogParser;
 import smile.clustering.DBSCAN;
 import smile.math.matrix.Matrix;
@@ -18,14 +16,18 @@ public class Prog {
 
     public static void main(String[] args) {
         try {
-            var res = Prog.class.getResource("/data/log-block.txt");
+            var res = Prog.class.getResource("/data/ConversationSet0.txt");
             if (res != null) {
                 convSet = new LogParser().parseFile(new File(res.getFile()));
             } else {
                 throw new IOException("L'URL de la ressource est null");
             }
-            Tableau t = TableauFactory.createTableau(convSet);
-            Matrix m = MatriceDistanceFactory.createMatrixDistance(t, new AbstacteurSession().abstracteur(convSet), 1, facteurAttenuation);
+            // Tableau t = TableauFactory.createTableau(convSet);
+            var sessionsAbstracts = new AbstacteurSession().abstracteur(convSet);
+            for (var x : sessionsAbstracts){
+                System.out.println("types: " + x.getTypage() + " refs:" + x.getRefs().size());
+            }
+            Matrix m = MatriceDistanceFactory.createMatrixDistance(sessionsAbstracts, 1, facteurAttenuation);
             System.out.println(m);
             var dbscan = DBSCAN.fit(m.toArray(), 1, 1);
             System.out.println("nombre de cluster(s) pour un facteur d'atténuation de " + facteurAttenuation + " : " + dbscan.k);
--- src/main/java/factory/MatriceDistanceFactory.java
+++ src/main/java/factory/MatriceDistanceFactory.java
@@ -1,38 +1,37 @@
 package factory;
 
 import model.SessionAbstract;
-import model.Tableau;
 import smile.math.matrix.Matrix;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MatriceDistanceFactory {
 
-    public static Matrix createMatrixDistance(Tableau t, List<SessionAbstract> sessionAbstracts, final int lookback, final double facteurAttenuation) {
-        return createMatrixDistance(t, sessionAbstracts, lookback, 1, facteurAttenuation);
+    public static Matrix createMatrixDistance(List<SessionAbstract> sessionAbstracts, final int lookback, final double facteurAttenuation) {
+        return createMatrixDistance(sessionAbstracts, lookback, 1, facteurAttenuation);
     }
 
-    public static Matrix createMatrixDistance(Tableau t, List<SessionAbstract> sessionAbstracts, final int lookback, final int incrementValue, final double facteurAttenuation) {
+    public static Matrix createMatrixDistance(List<SessionAbstract> sessionAbstracts, final int lookback, final int incrementValue, final double facteurAttenuation) {
         if (lookback < 0 || incrementValue < 0)
             throw new IllegalArgumentException("loopback ou incrementValue est négatif");
         if (facteurAttenuation <= 0 || facteurAttenuation > 1)
             throw new IllegalArgumentException("facteurAttenuation doit être compris entre 1 et 0");
-        Matrix m;
-        {
-            int tailleColumn = t.getSizeColumns();
-            m = new Matrix(tailleColumn, tailleColumn);
-        }
+
+        List<String> typageLabel = createLabelList(sessionAbstracts);
+        Matrix m = new Matrix(typageLabel.size(),typageLabel.size());
+
         for (SessionAbstract sabs : sessionAbstracts) {
             for (int i = 0; i < sabs.getTypage().size(); i++) {
                 String ref = sabs.getTypage().get(i);
-                int rowIndex = t.columnIndexOf(ref);
+                int rowIndex = getIndexOf(typageLabel,ref);
                 for (int j = 1; j <= lookback; j++) {
                     if (i >= j) {
                         String eventY = sabs.getTypage().get(i - j);
                         // on trouve l'emplacement où prendre et mettre la valeur
-                        int columnIndex = t.columnIndexOf(eventY);
+                        int columnIndex = getIndexOf(typageLabel,eventY);
                         // on obtient la distance
-                        double distanceValue = 1.0 / m.get(rowIndex, columnIndex) + incrementValue * Math.pow(facteurAttenuation, j);
+                        double distanceValue = m.get(rowIndex, columnIndex) + incrementValue * Math.pow(facteurAttenuation, j);
 
                         // 1/0 -> affichage valeur max d'un double
                         if (Double.POSITIVE_INFINITY == distanceValue) {
@@ -46,5 +45,26 @@ public class MatriceDistanceFactory {
             }
         }
         return m;
+    }
+
+    private static List<String> createLabelList(List<SessionAbstract> sessionAbstractList){
+        List<String> typageList = new ArrayList<>();
+        for (SessionAbstract s : sessionAbstractList){
+            for (String type : s.getTypage()){
+                getIndexOf(typageList,type);
+            }
+        }
+        return typageList;
+    }
+
+    private static int getIndexOf(List<String> typageLabel, String ref) {
+        int size = typageLabel.size();
+        for (int i = 0; i < size; i++) {
+            if (typageLabel.get(i).equals(ref)){
+                return i;
+            }
+        }
+        typageLabel.add(ref);
+        return size;
     }
 }
