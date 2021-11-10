41ca0a23f0a82e41cdaec8ba548b53654df53df7
Amiralgaby <gaby.theuws@gmail.com>
2021-11-10 10:07:42 +0100

    nouveau test matrix, log copy-paste

---
+++ src/test/java/MatrixTest2.java
@@ -0,0 +1,33 @@
+import factory.MatriceDistanceFactory;
+import model.AbstacteurSession;
+import model.ConversationSet;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import parser.LogParser;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MatrixTest2 {
+    @Test
+    public void testDistanceMatrixOnLogCopyPaste() {
+        var url = getClass().getResource("data/log-copy-paste.txt");
+        Assertions.assertNotNull(url);
+        try {
+            ConversationSet conversationSet = new LogParser().parseFile(new File(url.getFile()));
+            Assertions.assertNotNull(conversationSet);
+            var sessionsAbs = new AbstacteurSession().abstracteur(conversationSet);
+
+            for (var anAbstract : sessionsAbs) {
+                System.out.println("types: " + anAbstract.getTypage() + " refs:" + anAbstract.getRefs().size());
+            }
+
+            var m = MatriceDistanceFactory.createMatrixDistance(sessionsAbs,4,0.3);
+            System.out.println(m);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
